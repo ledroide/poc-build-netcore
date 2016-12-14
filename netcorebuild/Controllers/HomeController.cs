197f2f0847af134c52e9c0f0934c10ba61e50ca3
laurent.gilly <laurent.gilly@qxlricardo.fr>
2016-12-14 10:44:35 +0100

    Hello world netcore api

---
+++ netcorebuild/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace netcorebuild.Controllers
+{
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        // GET api/home/
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello world";
+        }
+    }
+}
